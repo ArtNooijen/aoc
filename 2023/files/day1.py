e6822d1494c276b04af761b3203a3c2d73c6c73e
Artnooijen <a.t.g.nooijen@gmail.com>
2023-12-05 15:39:10 +0100

    clearer code

--- 2023/files/day1.py
+++ 2023/files/day1.py
@@ -1,21 +1,37 @@
-input = open("day1p1.txt", "r").read().strip()
+# Open and read the file content, then strip any leading/trailing whitespace
+input_data = open("day1p1.txt", "r").read().strip()
 
-first_number = 0
-last_number = 0
+# Initialize variables for storing the sum of the first and last numbers
+sum_first_number = 0
+sum_last_number = 0
 
-for line in input.split("\n"):
+# Define an array to map words to their corresponding numeric values
+word_to_number = {
+    'one': '1', 'two': '2', 'three': '3', 'four': '4', 
+    'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+}
+
+# Iterate through each line in the input data
+for line in input_data.split("\n"):
     first_number_array = []
     last_number_array = []
-    word_array = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i ,c in enumerate(line):
-        if c.isdigit():
-            first_number_array.append(c)
-            last_number_array.append(c)
-        for j ,word in enumerate(word_array):
+
+    # Iterate over each character and its index in the line
+    for i, char in enumerate(line):
+        # Check if the character is a digit
+        if char.isdigit():
+            first_number_array.append(char)
+            last_number_array.append(char)
+        
+        # Check for word numbers in the line
+        for word, number in word_to_number.items():
             if line[i:].startswith(word):
-                last_number_array.append(str(j+1))
+                last_number_array.append(number)
 
-    first_number += int(first_number_array[0] + first_number_array[-1])
-    last_number += int(last_number_array[0] + last_number_array[-1])
+    # Calculate the sum of the first and last digits/numbers
+    sum_first_number += int(first_number_array[0] + first_number_array[-1])
+    sum_last_number += int(last_number_array[0] + last_number_array[-1])
 
-print(first_number, last_number)
+# Print the results
+print(sum_first_number, sum_last_number)
+
